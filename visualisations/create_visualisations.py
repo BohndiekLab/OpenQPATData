1e5070085dbaa293fcbcb30aa43a365975b14471
Gröhl <j.groehl@eni-g.de>
2025-06-26 13:34:08 +0200

    update SVOT visuals

--- visualisations/create_visualisations.py
+++ visualisations/create_visualisations.py
@@ -4,7 +4,7 @@ import pacfish as pf
 from utils.histogram_colorbar import add_histogram_colorbar
 
 PATH = r"W:\Group\Data\20250611_OpenQPATData/"
-DEVICE = "tropus"
+DEVICE = "svot"
 
 PATH_LABELS = f"{PATH}/{DEVICE}/labels/"
 PATH_RECON = f"{PATH}/{DEVICE}/recon/"
@@ -17,23 +17,26 @@ if DEVICE == "tropus":
     ASPECT = 0.244
 
 
-fig, ax = plt.subplots(1, 3, figsize=(8, 3), layout="constrained")
+fig, ax = plt.subplots(1, 4, figsize=(10.67, 3), layout="constrained")
 
 if DEVICE == "svot":
     labels = np.load(f"{PATH_LABELS}/{PHANTOM}.npz")["data"]
     recon = np.load(f"{PATH_RECON}/{PHANTOM}_{WAVELENGTH}.npy")
-    _data = np.max(recon, axis=1).T
+    _data = recon[180, :, :].T #np.max(recon, axis=1).T
     ax[0].imshow(_data, vmin=np.percentile(_data, 2), vmax=np.percentile(_data, 98))
     add_histogram_colorbar(ax[0], _data, label="p$_0$", color=COLOR)
-    ax[0].set_title("Reconstruction (x/z MIP)")
+    ax[0].plot([1, 359], [180, 180], color="red", linewidth=2)
+    ax[0].set_title("Reconstruction (y/z)")
+    ax[1].imshow(labels[180, :, :].T, cmap="magma")
+    ax[1].set_title("Reference Labels (y/z)")
 
-    _data = np.max(recon, axis=2).T
-    ax[1].imshow(_data, vmin=np.percentile(_data, 2), vmax=np.percentile(_data, 98))
-    add_histogram_colorbar(ax[1], _data, label="p$_0$", color=COLOR)
-
-    ax[1].set_title("Reconstruction (x/y MIP)")
-    ax[2].imshow(labels[:, :, 180].T, cmap="magma")
-    ax[2].set_title("Reference Labels")
+    _data = recon[:, :, 180].T #np.max(recon, axis=2).T
+    ax[2].imshow(_data, vmin=np.percentile(_data, 2), vmax=np.percentile(_data, 98))
+    add_histogram_colorbar(ax[2], _data, label="p$_0$", color=COLOR)
+    ax[2].plot([180, 180], [1, 359], color="red", linewidth=2)
+    ax[2].set_title("Reconstruction (x/y)")
+    ax[3].imshow(labels[:, :, 180].T, cmap="magma")
+    ax[3].set_title("Reference Labels (x/y)")
 else:
     labels = np.load(f"{PATH_LABELS}/{PHANTOM}.npy")
     recon = np.load(f"{PATH_RECON}/{PHANTOM}_{WAVELENGTH}.npy")
